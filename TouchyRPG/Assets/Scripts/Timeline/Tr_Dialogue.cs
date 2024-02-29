ac6a054d1fc519dd253faa7eb70705a9002fa454
guinala <guinala@alumni.uv.es>
2024-02-29 20:36:18 +0100

    Scripts para Timelines con diálogos (prototipo)
    Signed-off-by: guinala <guinala@alumni.uv.es>

---
+++ TouchyRPG/Assets/Scripts/Timeline/Tr_Dialogue.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+[TrackColor(30 / 255f, 30 / 255f, 200 / 255f)]
+[TrackBindingType(typeof(TextDialogue))]
+[TrackClipType(typeof(cl_Dialogue))]
+public class tr_dialogue : TrackAsset
+{
+
+}
